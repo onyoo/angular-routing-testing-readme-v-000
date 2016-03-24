ba5ebfb131cf6611c313d0c94cbed971f516e6c8
onyoo <roberto9m9@gmail.com>
2016-03-24 17:42:54 +0100

    Done.

--- tests/Routes.spec.js
+++ tests/Routes.spec.js
@@ -1,19 +1,61 @@
-describe('Routes', function () {
+describe('Routes', function() {
 	var $state;
 
 	beforeEach(module('app'));
-
-	beforeEach(inject(function ($injector) {
+	beforeEach(inject(function($injector) {
 		$state = $injector.get('$state');
 	}));
 
-	describe('Settings', function () {
+	describe('Settings', function() {
 		var state;
-
-		it('should have the correct URL', function () {
+		it('should have the correct URL', function() {
 			state = $state.get('settings');
-
 			expect(state.url).toEqual('/settings');
+		});
+		it('should have the correct template', function() {
+			expect(state.templateUrl).toEqual('views/settings.html')
+		})
+	});
+
+	describe('User', function() {
+		var state;
+		it('should have the correct URL for User profile', function() {
+			state = $state.get('settings.user');
+			expect(state.url).toEqual('/user')
+		});
+		it('should have the correct template', function() {
+			expect(state.templateUrl).toEqual('views/settings/user.html')
+		});
+	});
+
+	describe('Notifications', function() {
+		var state;
+		it('should have have the correct URL for the notifications page', function() {
+			state = $state.get('settings.notifications');
+			expect(state.url).toEqual('/notifications')
+		});
+		it('should access the propper template', function() {
+			expect(state.templateUrl).toEqual('views/settings/notifications.html');
 		});
 	});
 });
+//
+// describe('Routes', function () {
+// 	var $state;
+//
+// 	beforeEach(module('app'));
+//
+// 	beforeEach(inject(function ($injector) {
+// 		$state = $injector.get('$state');
+// 	}));
+//
+// 	describe('Settings', function () {
+// 		var state;
+//
+// 		it('should have the correct URL', function () {
+// 			state = $state.get('settings');
+//
+// 			expect(state.url).toEqual('/settings');
+// 		});
+// 	});
+// });
